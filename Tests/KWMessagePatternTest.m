cbb513463692e9c772a3d7d0d1e24a43aa876e70
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-04-26 22:50:28 +0300

    KWMessagePatternTest -> reverted the changes

--- Tests/KWMessagePatternTest.m
+++ Tests/KWMessagePatternTest.m
@@ -11,30 +11,16 @@
 
 #if KW_TESTS_ENABLED
 
-@interface KWMessagePatternAbstractTest : XCTestCase
-// override message pattern class to perform testing
-@property (nonatomic, readonly) Class messagePatternClass;
+@interface KWMessagePatternTest : XCTestCase
 
 @end
 
-@implementation KWMessagePatternAbstractTest
-
-@dynamic messagePatternClass;
-
-- (Class)messagePatternClass {
-    return nil;
-}
-
-- (void)performTest:(XCTestRun *)run {
-    if (![self isMemberOfClass:[KWMessagePatternAbstractTest class]]) {
-        [super performTest:run];
-    }
-}
+@implementation KWMessagePatternTest
 
 - (KWMessagePattern *)messagePatternWithSelector:(SEL)aSelector arguments:(id)firstArgument, ... {
     va_list argumentList;
     va_start(argumentList, firstArgument);
-    return [self.messagePatternClass messagePatternWithSelector:aSelector firstArgumentFilter:firstArgument argumentList:argumentList];
+    return [KWMessagePattern messagePatternWithSelector:aSelector firstArgumentFilter:firstArgument argumentList:argumentList];
 }
 
 - (void)testItShouldCreateMessagePatternsWithVarArgs {
@@ -98,7 +84,7 @@
     void *creationInvocationContext = nil;
     [creationInvocation setMessageArguments:&creationInvocationObserver, &creationInvocationKeyPath, &creationInvocationOptions, &creationInvocationContext];
     [creationInvocation setSelector:@selector(addObserver:forKeyPath:options:context:)];
-    KWMessagePattern *messagePattern = [self.messagePatternClass messagePatternFromInvocation:creationInvocation];
+    KWMessagePattern *messagePattern = [KWMessagePattern messagePatternFromInvocation:creationInvocation];
 
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
     [invocation setSelector:@selector(addObserver:forKeyPath:options:context:)];
@@ -119,7 +105,7 @@
     void *creationInvocationContext = nil;
     [creationInvocation setMessageArguments:&creationInvocationObserver, &creationInvocationKeyPath, &creationInvocationOptions, &creationInvocationContext];
     [creationInvocation setSelector:@selector(addObserver:forKeyPath:options:context:)];
-    KWMessagePattern *messagePattern = [self.messagePatternClass messagePatternFromInvocation:creationInvocation];
+    KWMessagePattern *messagePattern = [KWMessagePattern messagePatternFromInvocation:creationInvocation];
     NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
     [invocation setSelector:@selector(addObserver:forKeyPath:options:context:)];
     id observer = @"foo";
@@ -138,7 +124,7 @@
 	id creationInvocationHandler = [KWAny any];
 	[creationInvocation setMessageArguments:&creationInvocationMessage, &creationInvocationTimeInterval, &creationInvocationHandler];
 	[creationInvocation setSelector:@selector(speak:afterDelay:whenDone:)];
-	KWMessagePattern *messagePattern = [self.messagePatternClass messagePatternFromInvocation:creationInvocation];
+	KWMessagePattern *messagePattern = [KWMessagePattern messagePatternFromInvocation:creationInvocation];
 	NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
 	[invocation setSelector:@selector(speak:afterDelay:whenDone:)];
 	id message = @"Hello World";
@@ -181,51 +167,14 @@
 }
 
 - (void)testItShouldCompareMessagePatternsWithNilAndNonNilArgumentFilters {
-    KWMessagePattern *messagePattern1 = [self.messagePatternClass messagePatternWithSelector:@selector(setYear:)];
+    KWMessagePattern *messagePattern1 = [KWMessagePattern messagePatternWithSelector:@selector(setYear:)];
     NSArray *argumentFilters = @[[KWValue valueWithUnsignedInt:42]];
-    KWMessagePattern *messagePattern2 = [self.messagePatternClass messagePatternWithSelector:@selector(setYear:) argumentFilters:argumentFilters];
+    KWMessagePattern *messagePattern2 = [KWMessagePattern messagePatternWithSelector:@selector(setYear:) argumentFilters:argumentFilters];
 
     XCTAssertFalse([messagePattern1 isEqual:messagePattern2], @"expected message patterns to compare as not equal");
     XCTAssertFalse([messagePattern2 isEqual:messagePattern1], @"expected message patterns to compare as not equal");
 }
 
 @end
-
-@interface KWMessagePatternTest : KWMessagePatternAbstractTest
-
-@end
-
-@implementation KWMessagePatternTest
-
-- (Class)messagePatternClass {
-    return [KWMessagePattern class];
-}
-
-@end
-
-@interface KWBlockMessagePatternTest : KWMessagePatternAbstractTest
-
-@end
-
-@implementation KWBlockMessagePatternTest
-
-- (Class)messagePatternClass {
-    return [KWBlockMessagePattern class];
-}
-
-- (void)testItShouldHaveNullSelector {
-#define KWTestMessagePatternSelector(messagePattern) XCTAssertEqual([messagePattern selector], NULL)
-
-    KWTestMessagePatternSelector([[self.messagePatternClass alloc] init]);
-    KWTestMessagePatternSelector([[self.messagePatternClass alloc] initWithArgumentFilters:nil]);
-    KWTestMessagePatternSelector([self messagePatternWithSelector:@selector(description) arguments:nil]);
-    KWTestMessagePatternSelector([self.messagePatternClass messagePattern]);
-    KWTestMessagePatternSelector([self.messagePatternClass messagePatternWithArgumentFilters:nil]);
-    
-#undef KWTestMessagePatternSelector
-}
-
-@end
-
 
 #endif // #if KW_TESTS_ENABLED
