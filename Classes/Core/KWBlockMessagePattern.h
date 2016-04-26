11e92fb797bdbb9ba2b3bc2d5dde5c50bfe32b0c
Oleksa 'trimm' Korin <oleksandr.korin@idapgroup.com>
2016-04-26 21:59:07 +0300

    KWBlockMessagePattern -> refactored to subclass from KWAbstractMessagePattern

--- Classes/Core/KWBlockMessagePattern.h
+++ Classes/Core/KWBlockMessagePattern.h
@@ -6,17 +6,26 @@
 //  Copyright © 2016 Allen Ding. All rights reserved.
 //
 
-#import "KWMessagePattern.h"
+#import "KWAbstractMessagePattern.h"
 
-// Block signature doesn't have a selector as the second hidden argument, so block message pattern has selector = NULL
+@interface KWBlockMessagePattern : KWAbstractMessagePattern
 
-@interface KWBlockMessagePattern : KWMessagePattern
+#pragma mark - Initializing
 
-- (id)initWithArgumentFilters:(NSArray *)anArray;
-- (id)initWithFirstArgumentFilter:(id)firstArgumentFilter argumentList:(va_list)argumentList;
+- (id)initWithSignature:(NSMethodSignature *)signature;
+- (id)initWithSignature:(NSMethodSignature *)signature argumentFilters:(NSArray *)anArray;
+- (id)initWithSignature:(NSMethodSignature *)signature
+    firstArgumentFilter:(id)firstArgumentFilter
+           argumentList:(va_list)argumentList;
 
-+ (id)messagePattern;
-+ (id)messagePatternWithArgumentFilters:(NSArray *)anArray;
-+ (id)messagePatternWithFirstArgumentFilter:(id)firstArgumentFilter argumentList:(va_list)argumentList;
++ (id)messagePatternWithSignature:(NSMethodSignature *)signature;
++ (id)messagePatternWithSignature:(NSMethodSignature *)signature argumentFilters:(NSArray *)anArray;
++ (id)messagePatternWithSignature:(NSMethodSignature *)signature
+              firstArgumentFilter:(id)firstArgumentFilter
+                     argumentList:(va_list)argumentList;
+
+#pragma mark - Properties
+
+@property (nonatomic, readonly) NSMethodSignature   *signature;
 
 @end
